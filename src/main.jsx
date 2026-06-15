48eed43b9a30b7f251570890aaae458ac8dd1d23
Utkarsh Singh <utkarshsingh140971@gmail.com>
2026-06-15 12:03:46 +0530

    Add favicon and vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 import { ThemeProvider } from './components/theme-provider'
 
@@ -10,5 +11,6 @@ createRoot(document.getElementById('root')).render(
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <App />
     </ThemeProvider>
+    <Analytics />
   </StrictMode>,
 )
